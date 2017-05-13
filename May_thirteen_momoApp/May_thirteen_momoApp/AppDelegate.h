1fe23dc75bc2bf82c09516623261207c4e363c58
cxz <1604101058@qq.com>
2017-05-13 20:54:14 +0800

    初始化项目

---
+++ May_thirteen_momoApp/May_thirteen_momoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  May_thirteen_momoApp
+//
+//  Created by TouchWorld on 2017/5/13.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
